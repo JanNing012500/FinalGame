1408c65513c2a1ab8211f65928f48e2ddd27a05e
SamLuyen130 <66100661+SamLuyen130@users.noreply.github.com>
2021-06-07 15:12:44 -0700

    Final Main Update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /*
 Team Member: Neo Zhang, Sam Luyen, Jan Ning
-Title: 
-Completion Data: 
-Creative Tilt:
+Title: Tower Ascenscion  
+Completion Date: 6/6/2021
+Creative Tilt: Speedrunning Platformer with levels getting more difficult as time goes on
 
 */
 let gameOption = {
@@ -27,7 +27,7 @@ var config = {
     scene: [ Menu, room1, room2, room3, room4, room5, room6, room7,room8, room9],
     physics: {
         default: 'arcade',
-        arcade: {debug:1} // change false to make the pink lines go away
+        arcade: {debug:0} // change false to make the pink lines go away
     }
 };
 let game = new Phaser.Game(config);
